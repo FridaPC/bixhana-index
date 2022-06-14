f7606792ec580615a6652d74cd15dbc0008e0ab5
FridaPC <100498302+FridaPC@users.noreply.github.com>
2022-06-14 17:07:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var btn= document.getElementById('btn'),
+    caja= document.getElementById('caja'),
+    contador=1;
+
+
+function cambio ()
+
+{
+  if(contador==0)
+    {
+      
+      contador=1;
+      document.getElementById("btn").innerHTML= "traducir a  inglés";
+      document.getElementById("saludo").innerHTML= "Hola!";
+      document.getElementById("caja").innerHTML= "Soy la productora de este huevo y tengo un mensaje para ti.";
+      document.getElementById("nombre").innerHTML= "Inserta tu primer nombre:";
+      document.getElementById("btn2").innerHTML= "Descubre el mensaje";
+      
+    }
+  else {
+    
+    contador=0;
+    
+    document.getElementById("btn").innerHTML= "translate to spanish";
+    document.getElementById("saludo").innerHTML= "Hi!";
+    document.getElementById("caja").innerHTML= "I'm the producer of these eggs and I have a message for you.";
+    document.getElementById("nombre").innerHTML= "Insert your first name:";
+    document.getElementById("btn2").innerHTML= "Discover the message";
+      }
+}
+
+
+
+btn.addEventListener('click',cambio,false);
